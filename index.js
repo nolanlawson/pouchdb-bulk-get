38f649a0322de2c8321739afd971c26f07c6d8f1
Daniel Holth <dholth@fastmail.fm>
2014-11-14 10:37:02 -0500

    bugfixes

--- index.js
+++ index.js
@@ -7,59 +7,41 @@ exports.replicate = replicate.replicate;
 
 /*************************** HTTP Adapter *********************************/
 
-// Generate a URL with the host data given by opts and the given path
-function genDBUrl(opts, path) {
-  return genUrl(opts, opts.db + '/' + path);
-}
-
-// Generate a URL with the host data given by opts and the given path
-function genUrl(opts, path) {
-  if (opts.remote) {
-    // If the host already has a path, then we need to have a path delimiter
-    // Otherwise, the path delimiter is the empty string
-    var pathDel = !opts.path ? '' : '/';
-
-    // If the host already has a path, then we need to have a path delimiter
-    // Otherwise, the path delimiter is the empty string
-    return opts.protocol + '://' + opts.host + ':' + opts.port + '/' +
-           opts.path + pathDel + path;
-  }
-
-  return '/' + path;
-}
-
-// Get a bunch of documents by doing a bulk get for each set of 'GET' options in
-// a parameters array. Requires CouchDB API extension.
-exports.getBulk = utils.adapterFun('getBulk', function (parameters, callback) {
-  var options = {
-    method : "POST",
-    url : genDBUrl(host, '_bulk_get'),
-    headers : host.headers,
-    body : JSON.stringify({docs:parameters}),
-    json : true,
-    processData : true,
-    timeout : 10000,
-    cache : false
-  };
-
-  // Get the documents
-  ajax(options, function (err, doc, xhr) {
-    // If the document does not exist, send an error to the callback
-    if (err) {
-      return callback(err);
-    }
-
-    // Send the document to the callback
-    callback(null, doc, xhr);
-  });
-});
-
 function patchHTTPAdapter(PouchDB) { 
   var HTTPAdapter = PouchDB.adapters.http;
 
   var patchedAdapter = function(opts, callback) { 
     HTTPAdapter.call(this, opts, callback);
-    this.getBulk = exports.getBulk;
+
+    var ajaxOpts = opts.ajax || {};
+    opts = utils.clone(opts);
+    function ajax(options, callback) {
+      return utils.ajax(utils.extend({}, ajaxOpts, options), callback);
+    }
+
+    this.getBulk = utils.adapterFun('getBulk', function (parameters, callback) {
+      var options = {
+        method : "POST",
+        url : this.getUrl() + '_bulk_get',
+        headers : this.getHeaders(),
+        body : JSON.stringify({docs:parameters}),
+        json : true,
+        processData : true,
+        timeout : 10000,
+        cache : false
+      };
+
+      // Get the documents
+      ajax(options, function (err, doc, xhr) {
+        // If the document does not exist, send an error to the callback
+        if (err) {
+          return callback(err);
+        }
+
+        // Send the document to the callback
+        callback(null, doc, xhr);
+      });
+    });
   };
 
   extend(true, patchedAdapter, HTTPAdapter);
